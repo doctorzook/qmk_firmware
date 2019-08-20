6cf91c4a35365c685efa4fbda1f0fce441176679
Isaac Kunen <isaackunen@stdin.org>
2019-08-20 09:57:07 -0700

    Cleanup cruft in files.

--- keyboards/atreus62/keymaps/doctorzook/keymap.c
+++ keyboards/atreus62/keymaps/doctorzook/keymap.c
@@ -14,10 +14,12 @@
 #define _SPCL 4
 
 enum custom_keycodes {
-  IKK_TEST = SAFE_RANGE,
-	IKK_WIN_MODE,
-	IKK_MAC_MODE
+  WIN_MODE = SAFE_RANGE,
+	MAC_MODE
 };
+
+#define _ENDASH 0x2013 // Unicode en-dash
+#define _EMDASH 0x2014 // Unicode em-dash
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_DEFAULT] = { /* Qwerty base layer.*/
@@ -45,28 +47,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 },
 
 [_SPCL] = {
-		{ TO(_DEFAULT), IKK_WIN_MODE, IKK_MAC_MODE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, UC(8211) /* en-dash*/ },
-		{ KC_TRNS,      KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_P7,   KC_P8,   KC_P9,   KC_PSLS, UC(8212) /* em-dash*/ },
-		{ KC_TRNS,      KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_P4,   KC_P5,   KC_P6,   KC_PAST, KC_TRNS },
-		{ KC_TRNS,      KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_P1,   KC_P2,   KC_P3,   KC_PPLS, KC_TRNS },
-		{ MO(_RESET),   KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PEQL,  KC_TRNS, KC_P0,   KC_TRNS, KC_PDOT, KC_PMNS, KC_TRNS }
+		{ TO(_DEFAULT), WIN_MODE, MAC_MODE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, UC(_ENDASH) },
+		{ KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_P7,   KC_P8,   KC_P9,   KC_PSLS, UC(_EMDASH) },
+		{ KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_P4,   KC_P5,   KC_P6,   KC_PAST, KC_TRNS },
+		{ KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_P1,   KC_P2,   KC_P3,   KC_PPLS, KC_TRNS },
+		{ MO(_RESET),   KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_PEQL,  KC_TRNS, KC_P0,   KC_TRNS, KC_PDOT, KC_PMNS, KC_TRNS }
 },
-
-// [_XXXDEFAULT] = { /* qwerty */
-// 		{ KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_TRNS,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS },
-// 		{ KC_BSLS,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_TRNS,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RBRC },
-// 		{ KC_TAB,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_TRNS,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT },
-// 		{ KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_DELT,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LBRC },
-// 		{ KC_LCTL,  KC_LGUI, KC_LALT, KC_GRV,  MO(_NAV),KC_BSPC, KC_ENT,   KC_SPC,  KC_EQL,  KC_MINS, KC_QUOT, KC_ENT,  KC_RGUI }
-// },
-// 
-// [_XXXNAV] = { 
-// 		{ TO(_DEFAULT),  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11  },
-// 		{ KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_F12,  KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS },
-// 		{ KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS },
-// 		{ TO(_RESET),    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS },
-// 		{ KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS }
-// },
 
 [_RESET] = {
 		{ TO(_DEFAULT),  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   },
@@ -76,8 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		{ KC_NO  ,       KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , RESET }
 }
 
-
-/*
+/* For future cut-and-paste convenience.
 [_TRNS] = {
 		{ KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS },
 		{ KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS },
@@ -86,29 +71,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		{ KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS }
 },
 */
-};
-
-
-// IKK: So to be honest, I don't know what this does. I think it's old stuff from TMK; I may be able to remove it.
-const uint16_t PROGMEM fn_actions[] = {
-
-};
-
-// IKK: This is an old way to define macros... which I'm not using in this layout. Should probably nix it. 
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-	// MACRODOWN only works in this function
-	switch (id) {
-	case 0:
-		if (record->event.pressed) {
-			register_code(KC_RSFT);
-		}
-		else {
-			unregister_code(KC_RSFT);
-		}
-		break;
-	}
-	return MACRO_NONE;
 };
 
 void setWindowsMode(void) {
@@ -121,33 +83,28 @@ void setWindowsMode(void) {
 }
 
 void setMacMode(void) {
+	// This Unicode input mode requires going to 
+	//   System Preferences > Keyboard > Input Sources
+	// Adding Unicode Hex Input to the list (it's under Other), and 
+	// then activating it from the input dropdown in the Menu Bar.
 	set_unicode_input_mode(UC_OSX);
 	layer_on(_MAC);
 }
 
 // This gets run at startup
 void matrix_init_user(void) {
-	setWindowsMode(); // Start off in Windows mode by default
+	setWindowsMode();
 }
 
 // This should get run on each keypress.
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case IKK_TEST:
-      if (record->event.pressed) {
-				SEND_STRING(SS_TAP(X_T));
-				SEND_STRING(SS_TAP(X_E));
-				SEND_STRING("ST");
-      } else {
-        // NOP
-      }
-      return false; // Skip all further processing of this key
-		case IKK_WIN_MODE:
+		case WIN_MODE:
 			if (record->event.pressed) {
 				setWindowsMode();
 			}
-			return false;
-		case IKK_MAC_MODE:
+			return false; // Skip all further processing of this key
+		case MAC_MODE:
 			if (record->event.pressed) { 
 				setMacMode();
 			}
